4056b41c3cf5977c3f360f6aebbb59bf25c738df
itsmichaeld <67594883+itsmichaeld@users.noreply.github.com>
2023-01-01 13:30:09 +0000

    Added show more/less functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hi");
+const viewMoreBtn = document.getElementById("view-more-btn");
+const viewLessbtn = document.getElementById("view-less-btn");
+const initiallyHidden = document.getElementsByClassName("initially-hidden");
+
+// Functionality for view more/less articles
+
+viewMoreBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  for (blog of initiallyHidden) {
+    blog.style.display = "block";
+  }
+  viewMoreBtn.style.display = "none";
+  viewLessbtn.style.display = "block";
+});
+
+viewLessbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  for (blog of initiallyHidden) {
+    blog.style.display = "none";
+  }
+  viewMoreBtn.style.display = "block";
+  viewLessbtn.style.display = "none";
+});
